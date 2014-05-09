3271f02a42cec16e3ab29e4bbf2b3b43bb6c047d
Oliver Wang <mengmeng@codeaurora.org>
2014-05-09 12:13:19 +0800

    msm8916_32: remove barometer feature from package manager
    The barometer sensor does not exist in 8916 hardware and the sensor
    list.  Reporting this feature will cause the testSensorOperation
    CTS test failure. Thus remove it to fix the CTS issue.
    
    Change-Id: I6fd2e815d7fdfdd1e2dae884d570f6cf8fa085d8

--- msm8916_32.mk
+++ msm8916_32.mk
@@ -83,8 +83,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
 
 #fstab.qcom
 PRODUCT_PACKAGES += fstab.qcom
