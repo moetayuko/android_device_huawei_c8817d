025399c6bfa44dffa9e5b72a2745f2bf82b60af9
dianlujitao <dianlujitao@gmail.com>
2014-10-25 19:27:36 +0800

    c8817d: use gps.conf from vendor

--- c8817d.mk
+++ c8817d.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 LOCAL_PATH := device/huawei/c8817d
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
