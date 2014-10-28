1a2f2181ce66cf6abf896300904bf31842fc5de2
dianlujitao <dianlujitao@gmail.com>
2014-10-28 11:49:23 +0800

    c8817d: merge init.device.rc into init.qcom.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -106,14 +106,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.device.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.device.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.factory.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
