8878e3b9abe9f5dcc8f109b3d1cb7c6b11cac674
dianlujitao <dianlujitao@gmail.com>
2015-01-17 21:42:13 +0800

    c8817d: cm bring up

--- cm.mk
+++ cm.mk
@@ -15,13 +15,13 @@
 #
 
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/mk/config/cdma.mk)
+$(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := C8817D
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/c8817d/full_c8817d.mk)
@@ -32,4 +32,4 @@ PRODUCT_PACKAGES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := c8817d
-PRODUCT_NAME := mk_c8817d
+PRODUCT_NAME := cm_c8817d
