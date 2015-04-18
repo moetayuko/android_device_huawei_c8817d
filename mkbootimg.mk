7591e50bedf09b9b914da3f7fcc695a99267e6c2
dianlujitao <dianlujitao@gmail.com>
2015-04-18 19:27:33 +0800

    c8817d: change for integrated kernel build
    Change-Id: Ieb2e0f43f30a69d3e90da417849b5375203b56d9

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,11 +1,14 @@
-# Use prebuilt dt.img
+LOCAL_PATH := $(call my-dir)
+
+## Build and run dtbtool
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
-$(INSTALLED_DTIMAGE_TARGET):
-	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
-	$(call append-dtb)
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/dts/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+
 
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
@@ -22,4 +25,3 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
-	
